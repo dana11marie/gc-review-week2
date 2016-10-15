c51e9c2e82addc70491f530911893459811751e6
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-10-15 11:37:24 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// javascript syntax
+var statement = "I write JavaScript";
+
+//writing a function
+function writeStatement() {
+  console.log(statement);
+}
+
+//caling a function
+writeStatement();
+
+
+// function example
+//groupOrder = parameter
+
+function makePizza(groupOrder) {
+  groupOrder.forEach(function(order) {
+    var toppings = "";
+    order.toppings.forEach(function(topping, index, arr) {
+      if (index === arr.length-1) {
+        toppings += topping;
+      } else if (index === arr.length-2) {
+        toppings += topping + ", and ";
+      } else {
+        toppings += topping + ", ";
+      }
+    });
+  console.log(order.name + " ordered a " + toppings + " pizza with a " + order.crust + " crust!");
+  });
+}
+
+var orders = [
+  {
+    name: "Aisha",
+    crust: "cheesy pretzel",
+    toppings: ["goat cheese", "figs", "diced tomatoes"]
+  },
+  {
+    name: "Dana",
+    crust: "garlic",
+    toppings: ["pineapple", "feta"]
+  },
+  {
+    name: "Travis",
+    crust: "parmesan",
+    toppings: ["pepperoni", "bacon"]
+  },
+  {
+    name: "Gao",
+    crust: "cajun",
+    toppings: ["italian sausage", "jalapenos"]
+  }
+];
+
+//orders variable is the argument that gets passed into the function.
+makePizza(orders);
